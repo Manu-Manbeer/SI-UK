325d67b10ddb04f70664e1e926be057204d618ae
Manbeer Singh <manbeermakhloga@yahoo.com>
2015-04-12 11:20:02 +0530

    SI-UK
    Simple Program for Sorting Array

---
+++ SI-UK-F/SI-UK-F/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SI_UK_F
+{
+    class Program
+    {
+        //THIS IS CONSOLE APPLICATION FOR USR ENTER MIN 05 VALUES AND SORT THEM IN ASSCENDING ORDER
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t You should only Enter Numeric Values Only and Minimum 05 Values \n");
+            Console.Write("\t How many values you want to enter:");
+            try
+            {
+                int a = Convert.ToInt32(Console.ReadLine()); //Read User enter input 
+
+                if (a < 5) //CHECKING HERE THE NUMBER OF VALUES USER WANT TO SORT IF LESS THEN 5 THEN  DISPLAY MESSAGE AND BREAK
+                {
+                    
+                    Console.WriteLine("minimum five numbers you have to enter");
+
+                }
+                else 
+                {
+                    int[] _Array = new int[a]; //CREATE THE ARRAY BASED ON VALUE CHOOSEN BY THE USER
+                    int Current = 1;
+                    for (int i = 0; i < a; i++)
+                    {
+                        Console.Write("Enter  Value for {0}:Number  \t",Current);
+                        _Array[i] = Convert.ToInt32(Console.ReadLine()); //ASSIGN THE VALUES TO THE ARRAY 
+                        Current++;
+                    }
+
+                    Array.Sort(_Array); //USE INBUILD METHOD TO SORT THE ARRARY 
+                    Program.DisplayShortArray(_Array); // HERE ACCESS THE METHOD TO DISPLAY ARRARY VALUES AFTER SORTING 
+
+                }
+
+            }
+            catch (Exception ex) // IF USER ENTER OTHER THEN NUMERIC VALUES THEN EXCEPTION OCCURES AND MESSAGE DISPLAY TO USER
+            {
+                Console.WriteLine("Only numeric Values allowed");
+                Console.ReadLine();
+            }
+
+            Console.ReadLine();
+        }
+
+        static void DisplayShortArray(int[] _Array) //METHOD TO DISPLAY ARRARY AFTER SORTING 
+        {
+            Console.WriteLine("\n \t After sorting values are:");
+
+            foreach (int i in _Array)
+            {
+                Console.Write("\t{0}", i);
+            }
+           
+        }
+    }
+}
---
+++ SI-UK-S/SI-UK-S/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SI_UK_S
+{
+    //THIS IS CONSOLE APPLICATION WHICH IS ACCEPTING VALUE FOR STUDENT ARRARY(ROLL NUMBER AND MARKS) AND THEN DISPLAY STUDENT HIGEST MARKS
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nThis Console Application Accept User Input from User for Student(RollNumber and Marks)\n");
+
+            Console.WriteLine(" User Must have to inter details (with RollNumber and Markes Obtain) for 05 Students");
+
+            int [] _StudentRollNumber= new int[5]; //This Array store Student RollNumber
+            int [] _StudentMarks = new  int[5];   //This Array store Student Markes respect to RollNumber
+
+            int Number = 1; //This variable for only to track the number of student when user enter the details
+
+            Console.WriteLine("\n \t--- Please enter the detait for the Student ---- \n");
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("\t Enter Roll Number for the Student: {0} ", Number);
+                _StudentRollNumber[i] = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("\n\t Enter Marks for the Student: {0} ", Number);
+                _StudentMarks[i] = Convert.ToInt32(Console.ReadLine()); 
+
+                Number++; 
+            }
+
+            Array.Sort(_StudentMarks, _StudentRollNumber);
+
+            Console.WriteLine("Student RollNumber {0} having Heighest Markes {1} ",_StudentRollNumber[4],_StudentMarks[4]);
+            Console.ReadKey();  
+        }
+    }
+}
